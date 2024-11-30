a1383213d0d8de2ec9523f9c494321382134697f
MaRR1347 <mmigacz1347@gmail.com>
2024-11-30 19:49:41 +0100

    Add project files.

---
+++ MMSPProjekt/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MMSPProjekt
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<String> songs = new ObservableCollection<String>();
+        ObservableCollection<String> list = new ObservableCollection<String>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        
+
+            _ListView.ItemsSource = list;
+
+            list.Add("abecadło");
+            list.Add("z nieba spadło");
+        
+        
+        }
+        private void DodajEl(object sender, RoutedEventArgs e)
+        {
+            list.Add("Przycisk");
+        }
+
+        
+    }
+}
