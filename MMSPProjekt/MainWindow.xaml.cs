0a1e4943dae0b409bf94e97664f73028d21976b6
MaRR1347 <mmigacz1347@gmail.com>
2024-12-01 00:54:20 +0100

    functionality done

--- MMSPProjekt/MainWindow.xaml.cs
+++ MMSPProjekt/MainWindow.xaml.cs
@@ -1,14 +1,8 @@
 ﻿using System.Collections.ObjectModel;
-using System.Text;
+using System.Security.Cryptography;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace MMSPProjekt
 {
@@ -17,25 +11,86 @@ namespace MMSPProjekt
     /// </summary>
     public partial class MainWindow : Window
     {
-        ObservableCollection<String> songs = new ObservableCollection<String>();
-        ObservableCollection<String> list = new ObservableCollection<String>();
+        ObservableCollection<Object> songs = new ObservableCollection<Object>();
+        static ObservableCollection<Button> bttns = new ObservableCollection<Button>();
+        static ObservableCollection<Button> favs = new ObservableCollection<Button>();
         public MainWindow()
         {
             InitializeComponent();
-        
+            
+            _songs.ItemsSource = bttns;
+            _favs.ItemsSource = favs;
 
-            _ListView.ItemsSource = list;
-
-            list.Add("abecadło");
-            list.Add("z nieba spadło");
-        
-        
+            addSong("Livin on a prayer", "Bon Jovi", "3:30");
+            addSong("Livin on a prayer", "Bon Jovi", "3:30");
+            addSong("Livin on a prayer", "Bon Jovi", "3:30");
         }
+
+
         private void DodajEl(object sender, RoutedEventArgs e)
         {
-            list.Add("Przycisk");
+            String title = _title.Text;
+            String author = _author.Text;
+            String length = _length.Text;
+            if (title != "" && author != "" && length != "")
+            {
+                addSong(title, author, length);
+                _formHeader.Text = "Udało się";
+            }
+            else
+            {
+                _formHeader.Text = "Wystąpił błąd";
+            }
+        }
+        
+        public void addSong(String title, String author, String length)
+        {
+            Piosenka newSong = new Piosenka(title, author, length);
+            songs.Add(newSong);
         }
 
-        
+        public class Piosenka
+        {
+            public String title;
+            public String author;
+            public String length;
+            //private int songID;
+           
+
+            public Piosenka(String title, String author, String length)
+            {
+                this.title = title;
+                this.author = author;
+                this.length = length;
+                //this.songID = songID;
+               
+
+                Button button1 = new Button();
+                button1.Content = title + "|\t" + author + "|\t" + length;
+                button1.Width = 300;
+                button1.Height = 50;
+                button1.HorizontalAlignment = HorizontalAlignment.Left;
+
+                button1.Click += (sender, e) => addToFav(button1);
+                MainWindow.bttns.Add(button1);
+
+                bttns.Add(button1);
+
+            }
+
+            public void addToFav(Button button1)
+            {
+                Button button2 = new Button();
+                button2.Content = title + "|\t" + author + "|\t" + length;
+                button2.Width = 300;
+                button2.Height = 50;
+                button2.Click += (sender, e) => remFromFav(button2);
+                favs.Add(button2);
+            }
+            public void remFromFav(Button button2)
+            {
+                favs.Remove(button2);
+            }
+        }
     }
 }
